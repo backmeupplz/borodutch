b05dfa2581385cacf4dc919310580d53fcfb1564
backmeupplz <backmeupplz@gmail.com>
2019-06-03 20:10:08 -0700

    deploy

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template lang="pug">
+  v-app(dark)
+    p Hello there
+</template>
+
+<script lang="ts">
+export default {};
+</script>
---
+++ src/main.ts
@@ -0,0 +1,9 @@
+// Dependencies
+import Vue from 'vue'
+import App from './App.vue'
+
+Vue.config.productionTip = true
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
