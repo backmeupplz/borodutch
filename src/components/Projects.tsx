b21780914bfd0f37b99e292fc9b148d1d6afffd4
backmeupplz <backmeupplz@gmail.com>
2021-11-17 18:25:02 -0800

    add hide all button

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,12 +1,21 @@
 import { SubheaderText } from 'components/Text'
 import { Suspense } from 'react'
+import { appStore } from 'stores/AppStore'
 import { classnames } from 'classnames/tailwind'
+import { useSnapshot } from 'valtio'
+import Button from 'components/Button'
 import Loader from 'components/Loader'
 import Project from 'components/Project'
 import projects from 'data/projects'
 
 const projectPlaceholderContainer = classnames('pb-4', 'mx-4')
 const projectsContainer = classnames('flex', 'flex-col', 'gap-4')
+const headerContainer = classnames(
+  'flex',
+  'flex-row',
+  'justify-between',
+  'items-center'
+)
 
 function Loaders() {
   return (
@@ -27,9 +36,20 @@ function Loaders() {
 }
 
 export default function Projects() {
+  const appStoreSnapshot = useSnapshot(appStore)
   return (
     <>
-      <SubheaderText>Projects</SubheaderText>
+      <div className={headerContainer}>
+        <SubheaderText>Projects</SubheaderText>
+        {Object.keys(appStoreSnapshot.opened).length > 0 && (
+          <Button
+            onClick={() => {
+              appStore.opened = {}
+            }}
+            title="Hide all stats"
+          />
+        )}
+      </div>
       <Suspense fallback={<Loaders />}>
         <div className={projectsContainer}>
           {projects.map((project) => (
