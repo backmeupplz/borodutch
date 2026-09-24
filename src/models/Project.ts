9011ff53fb86119b5a8fda3c756d94064c332618
backmeupplz <backmeupplz@gmail.com>
2026-09-24 10:58:10 -0700

    GEN-29: list Plain Wallet among projects

--- src/models/Project.ts
+++ src/models/Project.ts
@@ -1,7 +1,7 @@
 import { ChartData } from 'react-frappe-charts'
 import type { ProjectCount } from 'helpers/projectsData'
 
-type ProjectCode = keyof ProjectCount | 'veydrift'
+type ProjectCode = keyof ProjectCount | 'veydrift' | 'plainwallet'
 
 export default interface Project {
   title: string
